c3e0c52b45aa381049a4d171f1cfdafcbe613417
MatthijsDV2004 <mdevries@csumb.edu>
2024-04-14 20:19:34 -0700

    cool an

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-function sayHello() {
-    alert("Hello from the external script!");
-  }
+const title = document.getElementById('title');
+
+title.addEventListener('mouseover', function() {
+    // Add the 'rotated' class to apply the rotation effect
+    this.classList.add('rotated');
+});
+
+title.addEventListener('mouseout', function() {
+    // Remove the 'rotated' class to reset the rotation effect
+    this.classList.remove('rotated');
+});

ea7b010c9f1ba8f5ac824ccefe29d53f8e2faca0
MatthijsDV2004 <mdevries@csumb.edu>
2024-04-14 20:22:30 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,6 @@
 const title = document.getElementById('title');
 
-title.addEventListener('mouseover', function() {
-    // Add the 'rotated' class to apply the rotation effect
-    this.classList.add('rotated');
-});
-
-title.addEventListener('mouseout', function() {
-    // Remove the 'rotated' class to reset the rotation effect
-    this.classList.remove('rotated');
+title.addEventListener('click', function() {
+    // Toggle the 'rotated' class to apply or remove the rotation effect on click
+    this.classList.toggle('rotated');
 });
